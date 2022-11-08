faee77976db3544afc19086493cfb1f01c639982
jorge.becerro <jorge.becerro@mimacom.com>
2022-11-08 19:09:47 +0100

    #1 - Initial commit

---
+++ src/main/java/io/github/jorgebg90/kafkastreamssbviz/KafkaStreamsSbVizApplication.java
@@ -0,0 +1,13 @@
+package io.github.jorgebg90.kafkastreamssbviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsSbVizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsSbVizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jorgebg90/kafkastreamssbviz/KafkaStreamsSbVizApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jorgebg90.kafkastreamssbviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsSbVizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
